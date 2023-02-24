140033b431b3dff5d633747830ecd3157e058b19
Milan <milanpaulson8@gmail.com>
2023-02-24 07:13:07 +0530

    hacked effect

---
+++ index.js
@@ -0,0 +1,22 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let interval = null
+
+document.querySelector("h1").onmouseover = event => {
+    let iterations = 0
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => {
+            if(index < iterations) {
+                return event.target.dataset.value[index]
+            }
+
+            return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("") 
+
+        if(iterations >= event.target.dataset.value.length) clearInterval(interval)
+
+        iterations += 1 / 3
+    }, 30)
+}
